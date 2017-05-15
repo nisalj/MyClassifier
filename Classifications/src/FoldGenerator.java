532632edb4fb99b364c2f225674b4a82f4982d22
Ben <phuajingan@gmail.com>
2017-05-15 14:22:13 +1000

    Success: DataReader and FoldGenerator

--- Classifications/src/FoldGenerator.java
+++ Classifications/src/FoldGenerator.java
@@ -7,61 +7,69 @@ import java.util.ArrayList;
  * 1. Takes in pima.txt and converts to pima-folds.txt
  * 2. Used by class Algorithm Accuracy to test for the percentage accuracy of each algorithm
  * 3. Creates file pima-folds.csv to be used in report
- * Input: filename(String)
+ * Input: ArrayList<double[]>
  * Output: ArrayList<ArrayList<double[]>>
  */
 
 public class FoldGenerator {
-//    ArrayList<ArrayList<double[]>> fold_all = null;
+    private ArrayList<ArrayList<double[]>> all_folds = null;
+    private ArrayList<double[]> data = null;
 
-    public static void main(String[] args) {
-        String filename = "pima.txt";
-        DataReader parser = new DataReader(filename);
-        ArrayList<double[]> data = parser.parseFile();
+    public FoldGenerator(ArrayList<double[]> data) {
+        this.data = data;
+    }
+
+    public ArrayList<ArrayList<double[]>> fgen() {
+//        String filename = "pima.txt";
+//        DataReader parser = new DataReader(filename);
+//        ArrayList<double[]> data = parser.parseFile();
         //create two classes containing all the data points with yes and no
         ArrayList<double[]> classY = new ArrayList<double[]>();
         ArrayList<double[]> classN = new ArrayList<double[]>();
-        int yes = 268;
-        int no = 500;
+        int yes = 0;
+        int no = 0;
 
         for(double[] entry: data) {
             if(entry[entry.length-1] == 1) {
+                yes++;
                 classY.add(entry); //add it to the class with yes's
             } else {
+                no++;
                 classN.add(entry); //add it to the class with no's
             }
         }
 
-        double yesPercent = (double)268/(double)768;
+        double yesPercent = (double)yes/(double)(yes+no);
 //        double noPercent = 500/768;
 
         //10-fold
         int baseEntries = data.size() / 10;
 //        int remainder = data.size() % 10;
 
-        ArrayList<double[]> fold = null;
+        ArrayList<double[]> one_fold = null;
 
         int Yptr = 0;
         int Nptr = 0;
-        ArrayList<ArrayList<double[]>> fold_all = new ArrayList<ArrayList<double[]>>(10);
+
+        all_folds = new ArrayList<ArrayList<double[]>>(10);
         for (int k = 0; k < 10; k++) {
-            fold = new ArrayList<>(100);
+            one_fold = new ArrayList<>(100);
             for (int i = 0; i < baseEntries; i++) {
                 if (i < (int) (yesPercent * baseEntries)) {
-                    fold.add(classY.get(Yptr));
+                    one_fold.add(classY.get(Yptr));
                     Yptr++;
                 } else {
-                    fold.add(classN.get(Nptr));
+                    one_fold.add(classN.get(Nptr));
                     Nptr++;
                 }
             }
-            fold_all.add(fold);
+            all_folds.add(one_fold);
         }
 
         //deal with remainders
         int it = 0;
         while (!classY.isEmpty() && Yptr < classY.size()) {
-            fold_all.get(it).add(classY.get(Yptr));
+            all_folds.get(it).add(classY.get(Yptr));
             it++;
             Yptr++;
         } it = 1;
@@ -73,7 +81,7 @@ public class FoldGenerator {
             e.printStackTrace();
         }
         assert printer != null;
-        for (ArrayList<double[]> foldItr:fold_all) {
+        for (ArrayList<double[]> foldItr:all_folds) {
             printer.println("fold" + it);
             for (double[] entry:foldItr) {
                 for (int i = 0; i < entry.length; i++) {
@@ -91,7 +99,7 @@ public class FoldGenerator {
             } printer.println(); it++;
         } printer.close();
 
-
+    return all_folds;
 //        testing: stratification of folds
 //        int nyes = 0;
 //        int nnos = 0;
--- Classifications/src/K_Nearest_Neighbour.java
+++ Classifications/src/K_Nearest_Neighbour.java
@@ -3,6 +3,8 @@
  * Runs the K-Nearest-Neighbour algorithm on it
  * Stores the accuracy
  * Stores the predicted yes/no
+ * Input: ArrayList<ArrayList<double[]>>
+ * Output: ArrayList<Entry></Entry>
  */
 public class K_Nearest_Neighbour {
 }
